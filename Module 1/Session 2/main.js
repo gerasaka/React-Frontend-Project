9a0d647747056973d23e2d351045da7854022d2a
gerasaka <gerryjulioo@gmail.com>
2021-07-09 17:43:46 +0700

    create api call

--- Module 1/Session 2/main.js
+++ Module 1/Session 2/main.js
@@ -1,3 +1,21 @@
+//form handle
 function createBtn(){
     alert(`Playlist ${document.getElementById('form-title').value} created!`);
 }
+
+//API url
+const detailSongUrl = "https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json";
+
+//API call
+async function getSongDetail(url) {
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(`API response = ${response.status}, ok = ${response.ok}`);
+    return data;
+}
+
+getSongDetail(detailSongUrl) 
+    .then(data => {
+        renderSongData(data);
+    })
+    .catch(reason => alert(reason.message))
